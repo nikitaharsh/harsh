a9e97e8f4216afc95d4e6f01162022c1a875c35c
nikitaharsh <140864546+nikitaharsh@users.noreply.github.com>
2023-07-29 17:37:26 +0530

    Update play.py

--- IroX/utils/inline/play.py
+++ IroX/utils/inline/play.py
@@ -58,7 +58,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="🥀sᴜᴘᴘᴏʀᴛ💫",
+                text="🌸sᴜᴘᴘᴏʀᴛ🌸",
                 url=f"https://t.me/+FhQ62ZERCqdiNzNl",
             ),
             InlineKeyboardButton(
@@ -120,7 +120,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="🥀sᴜᴘᴘᴏʀᴛ💫",
+                text="🌸sᴜᴘᴘᴏʀᴛ🌸",
                 url=f"https://t.me/+FhQ62ZERCqdiNzNl",
             ),
             InlineKeyboardButton(
@@ -153,7 +153,7 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="🥀sᴜᴘᴘᴏʀᴛ💫",
+                text="🌸sᴜᴘᴘᴏʀᴛ🌸",
                 url=f"https://t.me/+FhQ62ZERCqdiNzNl",
             ),
             InlineKeyboardButton(
@@ -183,7 +183,7 @@ def telegram_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="🥀sᴜᴘᴘᴏʀᴛ💫",
+                text="🌸sᴜᴘᴘᴏʀᴛ🌸",
                 url=f"https://t.me/+FhQ62ZERCqdiNzNl",
             ),
             InlineKeyboardButton(
@@ -342,7 +342,7 @@ def queue_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="🥀sᴜᴘᴘᴏʀᴛ💫",
+                text="🌸sᴜᴘᴘᴏʀᴛ🌸",
                 url=f"https://t.me/+FhQ62ZERCqdiNzNl",
             ),
             InlineKeyboardButton(
